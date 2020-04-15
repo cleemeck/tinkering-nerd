61134aa1267c13796f57c2df1d9dfc1f89c88319
Tomasz Klimek <tomasz.klimek@yahoo.pl>
2020-04-15 15:30:15 +0200

    added server to app

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import dash_bootstrap_components as dbc
 app = dash.Dash(__name__,
                 external_stylesheets=[dbc.themes.SUPERHERO],
                 suppress_callback_exceptions=True)
-
+server = app.server
